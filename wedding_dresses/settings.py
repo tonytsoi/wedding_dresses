2488a4d51e48307c30f2472854aaaf278080da4a
tonytsoi <52275528+tonytsoi@users.noreply.github.com>
2024-07-03 16:42:02 +0800

    Add image under Dress

--- wedding_dresses/settings.py
+++ wedding_dresses/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

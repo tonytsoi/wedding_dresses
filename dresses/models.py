898e33b0ab30f50712dd0ca2c066729cd5dbd827
tonytsoi <52275528+tonytsoi@users.noreply.github.com>
2024-07-03 16:17:13 +0800

    Set up model and admin site

--- dresses/models.py
+++ dresses/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    
+    def __str__(self):
+        return self.name
+    
+class Dress(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=True)
+    # image = models.ImageField(null=False, blank=False)
+    description = models.CharField(max_length=500, null=False, blank=False)
+    
+    def __str__(self):
+        return self.description
--- dresses/views.py
+++ dresses/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 def main(request):
-    # return HttpResponse("Hello world!")
     return render(request, "main.html")

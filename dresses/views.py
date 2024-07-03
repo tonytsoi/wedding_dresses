c76ef98cc75ea964d901e49c06d836d8097a7870
tonytsoi <52275528+tonytsoi@users.noreply.github.com>
2024-07-03 16:29:29 +0800

    Set up Brand and Dress

--- dresses/views.py
+++ dresses/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Brand, Dress
 
 # Create your views here.
 def main(request):
-    return render(request, "main.html")
+    brands = Brand.objects.all()
+    dresses = Dress.objects.all()
+    
+    context = {'brands': brands, 'dresses': dresses}
+    
+    return render(request, "main.html", context)

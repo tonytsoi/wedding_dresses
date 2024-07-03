cd5631ac6a4d4dd46bf767e779571538739589e7
tonytsoi <52275528+tonytsoi@users.noreply.github.com>
2024-07-03 16:06:47 +0800

    Render template main.html

--- dresses/views.py
+++ dresses/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def main(request):
-    return HttpResponse("Hello world!")
+    # return HttpResponse("Hello world!")
+    return render(request, "main.html")

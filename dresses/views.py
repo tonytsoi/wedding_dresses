bd0793bd0e46665c564eb64cb140ae4312e1d35b
tonytsoi <52275528+tonytsoi@users.noreply.github.com>
2024-07-03 17:07:26 +0800

    Dresses filtered for brand

--- dresses/views.py
+++ dresses/views.py
@@ -4,8 +4,14 @@ from .models import Brand, Dress
 
 # Create your views here.
 def main(request):
+    brand = request.GET.get('brand')
+    if brand == None:
+        dresses = Dress.objects.all()
+    else:
+        dresses = Dress.objects.filter(brand__name=brand)
+    
     brands = Brand.objects.all()
-    dresses = Dress.objects.all()
+    
     
     context = {'brands': brands, 'dresses': dresses}
     

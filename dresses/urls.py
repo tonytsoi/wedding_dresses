50be85d2d76f3411e858f5560a2f8c8edfe628d0
tonytsoi <52275528+tonytsoi@users.noreply.github.com>
2024-08-15 16:05:44 +0800

    delete bookings page

--- dresses/urls.py
+++ dresses/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('dress/<str:pk>/', views.viewDress, name='dress'),
+    path('^admin/', admin.site.urls),
 ]
